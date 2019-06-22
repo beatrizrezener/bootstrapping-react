b9f0422e2da4cd233688567bc3c684417479824a
Beatriz Rezener <beatrizrezener@gmail.com>
2019-06-22 12:17:26 -0300

    Completing Jumbotron Component with a Dynamic Fluid Child

--- src/bootstrap/jumbotron.js
+++ src/bootstrap/jumbotron.js
@@ -4,15 +4,18 @@ import Container from './container';
 
 export default class Jumbotron extends React.Component {
   render() {
+    const CSS_NAME = 'jumbotron';
     const props = Object.assign({}, this.props);
+    props.className = classify(CSS_NAME, props.className);
     const containerFluid = props.containerFluid;
     delete props.containerFluid;
-    console.log(containerFluid);
-
-    return <div {...props} className={classify('jumbotron', props.className)}>
+    
+    const body = props.className.includes(CSS_NAME+'-fluid') ? 
       <Container fluid={containerFluid}>
         {this.props.children}
-      </Container>
+      </Container> : this.props.children
+    return <div {...props} >
+      {body}
     </div>
   }
 }

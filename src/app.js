03b16bcab7c28e18ebde7a2dc9be5c69474f8b6a
Beatriz Rezener <beatrizrezener@gmail.com>
2019-06-22 14:35:57 -0300

    Building CSS files with Webpack

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Button from './bootstrap/button';
 import Jumbotron from './bootstrap/jumbotron';
+import './style.css';
 
 export default class App extends React.Component {
   render() {
-    return <Jumbotron className="-fluid text-sm-center" 
-    style={{ backgroundImage: 'url(assets/dog.jpg)',  color: 'white' }}
+    return <Jumbotron className="-fluid text-sm-center custom" 
     containerFluid={true} >
       <h1>React.js Interfaces</h1>
       <p>with Bootstrap and SASS.</p>
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -10,13 +12,30 @@ module.exports = {
     port:3000,
     publicPath: '/dist/'
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
-      }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', 
+          { 
+            loader: 'css-loader',
+            options: {
+              url: false 
+            }
+          }
+        ]
+      },
     ]
   },
 };

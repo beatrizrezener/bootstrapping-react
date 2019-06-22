0ef05afc476c4a3ca4211152987522b3b9f418b0
Beatriz Rezener <beatrizrezener@gmail.com>
2019-06-22 11:41:28 -0300

    Creating Jumbotron Component

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Button from './bootstrap/button';
+import Jumbotron from './bootstrap/jumbotron';
 
 export default class App extends React.Component {
   render() {
-    return <div className="jumbotron jumbotron-fluid text-sm-center" style={{ backgroundImage: 'url(assets/dog.jpg)', color: 'white'}} >
+    return <Jumbotron className="-fluid text-sm-center" style={{ backgroundImage: 'url(assets/dog.jpg)', color: 'white'}} >
     <div className="container">
       <h1>React.js Interfaces</h1>
       <p>with Bootstrap and SASS.</p>
@@ -13,6 +14,6 @@ export default class App extends React.Component {
       </p>
       <Button className="-primary -lg -block" disabled />
     </div>
-  </div>;
+  </Jumbotron>;
   }
 }
